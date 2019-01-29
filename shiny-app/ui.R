d5c6202655243e8945fe101f20288a226c5bf837
Shane Kercheval <shane.kercheval@gmail.com>
2019-01-28 20:22:02 -0800

    modified column names of simulated data

--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -30,15 +30,37 @@ shinyUI(fluidPage(theme="custom.css",
             'Load Dataset',
             tabsetPanel(type='tabs',
                 tabPanel(
-                    'Prior Data',
+                    'Attribution Windows',
                     tags$br(),
                     tags$p("A simulated dataset is loaded, but you may load your own, in the same format, via csv."),
+                    tags$br(),
+                    tags$p("This dataset contains, for each experiment and metric, the maximum number of days allowed between when a user first joins the experiment and when their conversion to the metric will be attributed to the associated experiment/variation."),
+                    tags$br(),
+                    fileInput(inputId='attribution_windows__uploadFile', "Load .csv:"),
+                    tags$br(),tags$br(),
+                    tabsetPanel(type='tabs',
+                        tabPanel(
+                            "First 500 Records of Attribution Windows",
+                            tags$div(class='results-table', dataTableOutput(outputId='attribution_windows__head_table'))
+                        ),
+                        tabPanel(
+                            'Variable Types',
+                            tags$div(class='results-table', dataTableOutput(outputId='attribution_windows__types_table'))
+                        )
+                    )
+                ),
+                tabPanel(
+                    'Website Traffic',
+                    tags$br(),
+                    tags$p("A simulated dataset is loaded, but you may load your own, in the same format, via csv."),
+                    tags$br(),
+                    tags$p("This dataset contains per-day website traffic. A single row represents one or more visits to a specific path (i.e. url) for a given user_id, on a given day."),
                     tags$br(),
                     fileInput(inputId='prior_data__uploadFile', "Load .csv:"),
                     tags$br(),tags$br(),
                     tabsetPanel(type='tabs',
                         tabPanel(
-                            "First 500 Records of Prior Dataset",
+                            "First 500 Records of Website Traffic",
                             tags$div(class='results-table', dataTableOutput(outputId='prior_data__head_table'))
                         ),
                         tabPanel(
@@ -48,20 +70,42 @@ shinyUI(fluidPage(theme="custom.css",
                     )
                 ),
                 tabPanel(
-                    'Experiment Results',
+                    'Experiment Traffic',
                     tags$br(),
                     tags$p("A simulated dataset is loaded, but you may load your own, in the same format, via csv."),
+                    tags$br(),
+                    tags$p("This dataset contains gives a list of user_ids and the experiments they have been part of, including the variation, the date they first joined the experiment, and the path/url they were on."),
                     tags$br(),
                     fileInput(inputId='experiment_data__uploadFile', "Load .csv:"),
                     tags$br(),tags$br(),
                     tabsetPanel(type='tabs',
                         tabPanel(
-                            "First 500 Records of Experiment Results",
+                            "First 500 Records of Experiment Traffic",
                             tags$div(class='results-table', dataTableOutput(outputId='experiment_data__head_table'))
                         ),
                         tabPanel(
                             'Variable Types',
                             tags$div(class='results-table', dataTableOutput(outputId='experiment_data__types_table'))
+                        )
+                    )
+                ),
+                tabPanel(
+                    'User Conversion Rates',
+                    tags$br(),
+                    tags$p("A simulated dataset is loaded, but you may load your own, in the same format, via csv."),
+                    tags$br(),
+                    tags$p("This dataset contains gives a list of user_ids and their conversion rate dates. If a user did not convert for a particular metric, the user_id/metric_id is not included in the dataset."),
+                    tags$br(),
+                    fileInput(inputId='conversion_rate_data__uploadFile', "Load .csv:"),
+                    tags$br(),tags$br(),
+                    tabsetPanel(type='tabs',
+                        tabPanel(
+                            "First 500 Records of User Conversion Rates",
+                            tags$div(class='results-table', dataTableOutput(outputId='conversion_rate_data__head_table'))
+                        ),
+                        tabPanel(
+                            'Variable Types',
+                            tags$div(class='results-table', dataTableOutput(outputId='conversion_rate_data__types_table'))
                         )
                     )
                 )
