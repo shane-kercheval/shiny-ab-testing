c5d332605910736ac797d708bb175b806e4f2ee2
Shane Kercheval <shane.kercheval@gmail.com>
2019-02-03 10:09:17 -0800

    Update test_helpers.R

--- unit_tests/test_helpers.R
+++ unit_tests/test_helpers.R
@@ -18,11 +18,48 @@ source('unit_test_helpers.R')
     # experiment_traffic <- as.data.frame(read_csv('data/cached_simulated_data/experiment_traffic.csv'))
     # conversion_rate_data <- as.data.frame(read_csv('data/cached_simulated_data/conversion_rate_data.csv'))
 
-
-
 ##############################################################################################################
 # Misc Helpers
 ##############################################################################################################
+test_that('prettify_numerics', {
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=0, sd=0.001)),
+                 c("1.37e-03", "-5.65e-04", "3.63e-04", "6.33e-04", "4.04e-04", "-1.06e-04", "1.51e-03", "-9.47e-05", "2.02e-03", "-6.27e-05"))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=0, sd=0.1)),
+                 c(0.14, -0.06, 0.04, 0.06, 0.04, -0.01, 0.15, -0.01, 0.20, -0.01))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=0)),
+                 c(1.4, -0.6, 0.4, 0.6, 0.4, -0.1, 1.5, -0.1, 2.0, -0.1))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=10)),
+                 c(11.4, 9.4, 10.4, 10.6, 10.4, 9.9, 11.5, 9.9, 12.0, 9.9))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=100, sd=10)),
+                 c(114, 94, 104, 106, 104, 99, 115, 99, 120, 99))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=1000, sd=100)),
+                 c("1.14K", "0.94K", "1.04K", "1.06K", "1.04K", "0.99K", "1.15K", "0.99K", "1.2K", "0.99K"))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=10000, sd=1000)),
+                 c("11.4K", "9.4K", "10.4K", "10.6K", "10.4K", "9.9K", "11.5K", "9.9K", "12K", "9.9K"))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=100000, sd=10000)),
+                 c("113.7K", "94.4K", "103.6K", "106.3K", "104K", "98.9K", "115.1K", "99.1K", "120.2K", "99.4K" ))
+
+    set.seed(42)
+    expect_equal(prettify_numerics(rnorm(n=10, mean=1000000, sd=100000)),
+                 c("1.14M", "0.94M", "1.04M", "1.06M", "1.04M", "0.99M", "1.15M", "0.99M", "1.2M", "0.99M"))
+})
+
 test_that("Misc Helpers: calculate_total_sample_size", {
 
     result <- calculate_total_sample_size(original_conversion_rate=0.30,
@@ -185,43 +222,4 @@ test_that("test_helpers: website_traffic__plot_traffic", {
                                                  filter_year_end_beginning_weeks=TRUE,
                                                  top_n_paths=3)
     plot_object %>% test_save_plot(file='data/plot_helpers/website_traffic__plot_traffic/first-visits-weekly-filter_top_3_paths.png')
-})
-
-test_that('prettify_numerics', {
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=0, sd=0.001)),
-                 c("1.37e-03", "-5.65e-04", "3.63e-04", "6.33e-04", "4.04e-04", "-1.06e-04", "1.51e-03", "-9.47e-05", "2.02e-03", "-6.27e-05"))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=0, sd=0.1)),
-                 c(0.14, -0.06, 0.04, 0.06, 0.04, -0.01, 0.15, -0.01, 0.20, -0.01))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=0)),
-                 c(1.4, -0.6, 0.4, 0.6, 0.4, -0.1, 1.5, -0.1, 2.0, -0.1))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=10)),
-                 c(11.4, 9.4, 10.4, 10.6, 10.4, 9.9, 11.5, 9.9, 12.0, 9.9))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=100, sd=10)),
-                 c(114, 94, 104, 106, 104, 99, 115, 99, 120, 99))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=1000, sd=100)),
-                 c("1.14K", "0.94K", "1.04K", "1.06K", "1.04K", "0.99K", "1.15K", "0.99K", "1.2K", "0.99K"))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=10000, sd=1000)),
-                 c("11.4K", "9.4K", "10.4K", "10.6K", "10.4K", "9.9K", "11.5K", "9.9K", "12K", "9.9K"))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=100000, sd=10000)),
-                 c("113.7K", "94.4K", "103.6K", "106.3K", "104K", "98.9K", "115.1K", "99.1K", "120.2K", "99.4K" ))
-
-    set.seed(42)
-    expect_equal(prettify_numerics(rnorm(n=10, mean=1000000, sd=100000)),
-                 c("1.14M", "0.94M", "1.04M", "1.06M", "1.04M", "0.99M", "1.15M", "0.99M", "1.2M", "0.99M"))
 })
